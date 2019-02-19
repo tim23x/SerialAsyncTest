7430cf6c2731ea21eeecaf321f2019bf1b14fca9
tim23x <tim23x@hotmail.com>
2019-02-19 10:30:41 +0000

    Removed dead code

--- Interface/MainWindow.xaml.cs
+++ Interface/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Concurrent;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
 using System.IO.Ports;
 using System.Diagnostics;
 using OxyPlot;
@@ -52,15 +40,12 @@ namespace Interface
         public void GetData(ConcurrentQueue<string> qData, CancellationToken token, String portName)
         {
             SerialPort port = SerialConnect(portName);
-            //leftPort.DataReceived += new SerialDataReceivedEventHandler(LeftDataReceivedHandler); 
             while (true)
             {
-                //token.ThrowIfCancellationRequested();
                 if (token.IsCancellationRequested)
                     break;
                 if (port.BytesToRead > 0)
                 {
-                    //string received = port.ReadExisting();
                     string received = port.ReadTo("#");
                     qData.Enqueue(received);
                 }
@@ -72,7 +57,6 @@ namespace Interface
             string local;
             while (true)
             {
-                //token.ThrowIfCancellationRequested();
                 if (token.IsCancellationRequested)
                     break;
                 while (qData.TryDequeue(out local))
@@ -92,106 +76,7 @@ namespace Interface
                 Thread.Sleep(1);
             }
         }
-        /*
-        public void AddLeftData(ConcurrentQueue<string> qData, CancellationToken token)
-        {
-            string local;
-            while (true)
-            {
-                //token.ThrowIfCancellationRequested();
-                if (token.IsCancellationRequested)
-                    break;
-                while (qData.TryDequeue(out local))
-                {
-                    String[] data = local.Split('#');
-                    foreach (string point in data)
-                    {
-                        try
-                        {
-                            DataPoint dp = new DataPoint(xTime.ElapsedMilliseconds, Convert.ToDouble(point));
-                            plotLeft.AddDataPointToSeries(0, dp);
-                        }
-                        catch (Exception ex)
-                        { }
-                    }
-                }
-                Thread.Sleep(1);
-            }
-        }
 
-
-        public void GetRightData(ConcurrentQueue<string> qData, CancellationToken token, SerialPort rightPort)
-        {
-            rightPort = SerialConnect("COM9");
-            //rightPort.DataReceived += new SerialDataReceivedEventHandler(RightDataReceivedHandler);
-            if (rightPort.BytesToRead > 0)
-            {
-                string received = rightPort.ReadExisting();
-
-                qData.Enqueue(received);
-            }
-        }
-
-        private void AddRightData(ConcurrentQueue<string> qData, CancellationToken token)
-        {
-            string local;
-            while (true)
-            {
-                //token.ThrowIfCancellationRequested();
-                if (token.IsCancellationRequested)
-                    break;
-                while (qData.TryDequeue(out local))
-                {
-                    String[] data = local.Split('#');
-                    foreach (string point in data)
-                    {
-                        try
-                        {
-                            DataPoint dp = new DataPoint(xTime.ElapsedMilliseconds, Convert.ToDouble(point));
-                            plotRight.AddDataPointToSeries(0, dp);
-                        }
-                        catch (Exception ex)
-                        { }
-                    }
-                }
-                Thread.Sleep(1);
-            }
-        }
-
-        public void GetLeftData(ConcurrentQueue<string> qData, CancellationToken token, String portName)
-        {
-            SerialPort port = SerialConnect(portName);
-            //leftPort.DataReceived += new SerialDataReceivedEventHandler(LeftDataReceivedHandler); 
-            while (true)
-            {
-                //token.ThrowIfCancellationRequested();
-                if (token.IsCancellationRequested)
-                    break;
-
-                if (port.BytesToRead > 0)
-                {
-                    string received = port.ReadExisting();
-                    qData.Enqueue(received);
-                }
-            }
-        }
-
-        private void LeftDataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
-        {
-            var s = sender as SerialPort;
-            string received = s.ReadExisting();
-            lData.Enqueue(received);
-        }
-
-        private void RightDataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
-        {
-            var s = sender as SerialPort;
-            string received = s.ReadExisting();
-            rData.Enqueue(received);
-        }
-
-
-    */
         private SerialPort SerialConnect(String name)
         {
             SerialPort port = new SerialPort();
